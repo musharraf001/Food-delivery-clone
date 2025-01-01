678f7cc8edba949facd77f3496637c85dd8b4e31
musharraf001 <mushusk10@gmail.com>
2025-01-01 15:58:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function scrollToMenu() {
+    const menuSection = document.getElementById("menu");
+    menuSection.scrollIntoView({ behavior: "smooth" });
+}
+
+// You can add more JavaScript functionalities here in the future
